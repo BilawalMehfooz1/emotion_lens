0c1eeb91a1b790f85ecbdd3a6a127d6537a14b7d
Bilawal Mehfooz <bilawalmehfoozmalik@gmail.com>
2023-08-13 20:00:24 +0500

    added map preview functionality

--- lib/models/place_structure.dart
+++ lib/models/place_structure.dart
@@ -4,12 +4,25 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+class PlaceLocation {
+  const PlaceLocation({
+    required this.latitude,
+    required this.longitude,
+    required this.address,
+  });
+  final double latitude;
+  final double longitude;
+  final String address;
+}
+
 class PlaceStructure {
   PlaceStructure({
     required this.title,
     required this.image,
+    // required this.location,
   }) : id = uuid.v4();
   final String id;
   final String title;
   final File image;
+  // final PlaceLocation location;
 }
--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 import 'package:http/http.dart' as http;
+import 'package:emotion_lens/models/place_structure.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -12,8 +13,18 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
-  Location? _pickedLocation;
+  PlaceLocation? _pickedLocation;
   var _isGettingLocation = false;
+
+  String get locationImage {
+    if (_pickedLocation == null) {
+      return '';
+    }
+    final lat = _pickedLocation!.latitude;
+    final lng = _pickedLocation!.longitude;
+
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyClPM4aaxlYFz2qD6gEZQjs8zSvG4e2V-g';
+  }
 
   void _getCurrentLocation() async {
     Location location = Location();
@@ -29,7 +40,6 @@ class _LocationInputState extends State<LocationInput> {
         return;
       }
     }
-
     permissionGranted = await location.hasPermission();
     if (permissionGranted == PermissionStatus.denied) {
       permissionGranted = await location.requestPermission();
@@ -37,20 +47,27 @@ class _LocationInputState extends State<LocationInput> {
         return;
       }
     }
-
     setState(() {
       _isGettingLocation = true;
     });
     locationData = await location.getLocation();
     final lat = locationData.latitude;
     final lng = locationData.longitude;
+    if (lat == null || lng == null) {
+      return;
+    }
     final url = Uri.parse(
         'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=AIzaSyClPM4aaxlYFz2qD6gEZQjs8zSvG4e2V-g');
     final response = await http.get(url);
     final resData = json.decode(response.body);
     final address = resData['results'][0]['formatted_address'];
     setState(() {
-      _isGettingLocation = true;
+      _pickedLocation = PlaceLocation(
+        latitude: lat,
+        longitude: lng,
+        address: address,
+      );
+      _isGettingLocation = false;
     });
   }
 
@@ -63,6 +80,15 @@ class _LocationInputState extends State<LocationInput> {
 
     if (_isGettingLocation) {
       content = const CircularProgressIndicator();
+    }
+
+    if (_pickedLocation != null) {
+      content = Image.network(
+        locationImage,
+        fit: BoxFit.cover,
+        width: double.infinity,
+        height: double.infinity,
+      );
     }
     return Column(
       children: [
