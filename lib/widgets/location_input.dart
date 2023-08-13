a46bceeeac6867432fb86fbcfef03bdcdee36218
Bilawal Mehfooz <bilawalmehfoozmalik@gmail.com>
2023-08-13 16:55:05 +0500

    added location method

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -8,8 +9,52 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
+  Location? _pickedLocation;
+  var _isGettingLocation = false;
+
+  void _getCurrentLocation() async {
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    setState(() {
+      _isGettingLocation = true;
+    });
+    locationData = await location.getLocation();
+    print(locationData.longitude);
+    setState(() {
+      _isGettingLocation = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget content = const Text(
+      'No location chosen',
+      textAlign: TextAlign.center,
+    );
+
+    if (_isGettingLocation) {
+      content = const CircularProgressIndicator();
+    }
     return Column(
       children: [
         Container(
@@ -22,16 +67,13 @@ class _LocationInputState extends State<LocationInput> {
               color: Colors.black26,
             ),
           ),
-          child: const Text(
-            'No location chosen',
-            textAlign: TextAlign.center,
-          ),
+          child: content,
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             TextButton.icon(
-              onPressed: () {},
+              onPressed: _getCurrentLocation,
               icon: const Icon(Icons.location_on),
               label: const Text('Get Current Location'),
             ),
