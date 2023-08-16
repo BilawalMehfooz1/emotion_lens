472102179ef2615cbef5a800c04a5a601e02c28b
Bilawal Mehfooz <bilawalmehfoozmalik@gmail.com>
2023-08-16 10:02:53 +0500

    added circular avatar on detail page

--- lib/screens/place_detail_screen.dart
+++ lib/screens/place_detail_screen.dart
@@ -9,20 +9,67 @@ class PlaceDetailsScreen extends StatelessWidget {
   });
 
   final PlaceStructure place;
+
+  String get locationImage {
+    final lat = place.location.latitude;
+    final lng = place.location.longitude;
+
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyClPM4aaxlYFz2qD6gEZQjs8zSvG4e2V-g';
+  }
+
   @override
   Widget build(BuildContext context) {
+    final style = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text(place.title),
       ),
-      body: Stack(children: [
-        Image.file(
-          place.image,
-          fit: BoxFit.cover,
-          height: double.infinity,
-          width: double.infinity,
-        )
-      ]),
+      body: Stack(
+        children: [
+          Image.file(
+            place.image,
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 70,
+                  backgroundImage: NetworkImage(locationImage),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 16,
+                    horizontal: 24,
+                  ),
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.transparent,
+                        Colors.black87,
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Text(
+                    place.location.address,
+                    textAlign: TextAlign.center,
+                    style: style.textTheme.titleMedium!.copyWith(
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
